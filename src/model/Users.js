0d66b397f9e5f618b4290ac80c9dc7f864c13a0a
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2024-11-23 16:42:18 -0300

    corrigindo assets

--- src/model/Users.js
+++ src/model/Users.js
@@ -1,2 +1,5 @@
-const pool = require("../src/connection.js");
+const pool = require("../db/connection.js");
 
+async function AddUser(name, email, password, address){
+    const result = await pool.query()
+}
