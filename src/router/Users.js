ab7737b632d52e7781b81c27559fc42e857c4b90
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2024-11-24 18:28:33 -0300

    Funcionalidade de login de usuario EM ANDAMENTO

--- src/router/Users.js
+++ src/router/Users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const UserController = require("../controller/Users");
 
 router.post('/users', UserController.AddUserController);
+router.post('/loginUsers', UserController.PostUserController);
 
 module.exports = router;
