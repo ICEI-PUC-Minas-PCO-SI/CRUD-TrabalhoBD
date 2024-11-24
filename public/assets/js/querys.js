818a183481ad9a83430aa19a4662bf69ff16a078
Domiciano Vieira <dgnvieira@sga.pucminas.br>
2024-11-24 14:37:11 -0300

    Querys atualizadas

--- public/assets/js/querys.js
+++ public/assets/js/querys.js
@@ -11,14 +11,14 @@ const userTable = `CREATE TABLE IF NOT EXISTS usuarios (
 
 const orderTable = `CREATE TABLE IF NOT EXISTS pedidos (
                           id_pedido SERIAL PRIMARY KEY,
-                          data DATE,
+                          data TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
                           valor_total DECIMAL,
                           id_usuario INT NOT NULL,
                           id_combinacao INT NOT NULL,
                           FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
                           FOREIGN KEY (id_combinacao) REFERENCES combinacoes(id_combinacao) ON DELETE CASCADE
-                           );
-                          `;
+                           );`
+                           ;
 
 const combinationTable = `CREATE TABLE IF NOT EXISTS combinacoes (
                                 id_combinacao SERIAL PRIMARY KEY,
@@ -30,17 +30,27 @@ const combinationTable = `CREATE TABLE IF NOT EXISTS combinacoes (
                                  );
                                 `;
 
+const selectAllOrders = `SELECT *FROM pedidos`;                                
+
 const createUser = `INSERT INTO usuarios (id_usuario, nome, endereco, email, senha) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
-const createOrder = `INSERT INTO pedidos (id_pedido, data, valor_total, id_usuario, id_combinacao) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+const createOrder = `INSERT INTO pedidos (valor_total, id_usuario, id_combinacao) VALUES ($1, $2, $3) RETURNING *`;
 const createCombination = `INSERT INTO combinacoes (id_combinacao, preco, sabor_massa, sabor_recheio, tamanho, descricao_decoracao) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
 
-const updateOrder = `UPDATE pedidos SET
+const updateOrder = `UPDATE pedidos
                      SET
                         data = $1,
                         valor_total = $2
                         WHERE id_pedido = $3
                     `
-const deleteOrder = `DELETE FROM pedidos WHERE id_pedido = $1`
+const deleteOrder = `DELETE FROM pedidos WHERE id_pedido = $1`;
+
+const selectCombinationId = `SELECT id_combinacao
+      FROM combinacoes
+      WHERE sabor_massa = $1
+        AND sabor_recheio = $2
+        AND tamanho = $3
+        AND descricao_decoracao = $4;
+    `
 
 const createTable = async (tableName) => {
     try {
@@ -65,7 +75,7 @@ const createTable = async (tableName) => {
 };
 
 const checkTableExistence = async (tableName) => {
-    const result = await db.query(`SELECT to_regclass($1);`, [tableName]);
+    const result = await db.query(`SELECT to_regclass($1); `, [tableName]);
     return result.rows[0].to_regclass !== null;
 };
 
@@ -83,7 +93,7 @@ const addUser = async (user) => {
 const addOrder = async (order) => {
     const { orderId, date, finalValue, userId, combinationId } = order;
     try {
-        const result = await db.query(createOrder, [orderId, date, finalValue, userId, combinationId]);
+        const result = await db.query(createOrder, [finalValue, userId, combinationId]);
         console.log("Pedido criado com sucesso");
         return result.rows[0];
     } catch (error) {
@@ -102,23 +112,23 @@ const addCombination = async (combination) => {
     }
 };
 
-const deleteOrderById = async(orderId) => {
-    if(!checkIfExists) return;
+const deleteOrderById = async (orderId) => {
+    if (!checkIfExists) return;
     try {
         const result = await db.query(deleteOrder, [orderId]);
         console.log(`Pedido ${orderId} deletado com sucesso`);
-    }catch(error) {
-        console.error(`Erro ao deletar pedido ${orderId}`);
+    } catch (error) {
+        console.error(`Erro ao deletar pedido ${orderId} `, error.message);
     }
 };
 
-const updateOrderById = async(date, value, orderId) => {
-    if(!checkIfExists) return;
+const updateOrderById = async (date, value, orderId) => {
+    if (!checkIfExists) return;
     try {
         const result = await db.query(updateOrder, [date, value, orderId]);
         console.log("Pedido editado com sucesso");
-    }catch(error) {
-        console.error(`Erro ao editar pedido ${orderId}`, error.message);
+    } catch (error) {
+        console.error(`Erro ao editar pedido ${orderId} `, error.message);
     }
 };
 
@@ -132,3 +142,20 @@ const checkIfExists = async (tableName, idColumns, idValue) => {
         console.error(`Erro ao verificar a existência do elemento na tabela ${tableName} `, error.message);
     }
 };
+
+const checkCombination = async (base, filling, size, description) => {
+    try {
+        const result = await db.query(selectCombinationId, [base, filling, size, description]);
+    } catch (error) {
+        console.error("Erro ao buscar combinação", error.message);
+    }
+};
+
+const getAllOrders = async() => {
+    try {
+        const result = await db.query(selectAllOrders);
+        return result.rows;
+    }catch(error) {
+        console.error("Erro ao exibir todos os pedidos");
+    }
+}
